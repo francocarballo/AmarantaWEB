cb493d4f812f15e5114d47fb0bd842f979812859
Franco Carballo <franco_carb@hotmail.com>
2017-01-03 17:03:10 -0300

    Add files via upload

---
+++ AmarantaWEB/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    console.log('its working !');
+
+    $(document).ready(function () {
+    $('.carousel').carousel({
+        interval: 9000
+    });
+
+    $('.carousel').carousel('cycle');
+    });
+})
