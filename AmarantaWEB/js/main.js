dc02ff0bcc955168598844528ac1ec59c33a1ca6
Francisco F Reynolds <freynolds@gmail.com>
2017-01-10 20:21:20 -0300

    [Fotos] temporal, + cambios en altura

--- AmarantaWEB/js/main.js
+++ AmarantaWEB/js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    console.log('its working !');
+	console.log('its working !');
 
-    $(document).ready(function () {
-    $('.carousel').carousel({
-        interval: 9000
-    });
+	$(document).ready(function () {
+		$('.carousel').carousel({
+			interval: 9000
+		});
 
-    $('.carousel').carousel('cycle');
-    });
+		$('.carousel').carousel('cycle');
+	});
 })
